a7c3fccf016fa29c802f940048479aa7e63e9dcc
Mikba883 <81826986+Mikba883@users.noreply.github.com>
2025-10-01 06:59:08 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,11 @@
+import { initGTM } from './lib/gtm';
+
+const GTM_ID = import.meta.env.VITE_GTM_ID;
+
+if (import.meta.env.PROD) {
+  initGTM(GTM_ID);
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
